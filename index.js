938bc77376125eab5edb5134d225e8a3db83131c
Diane Vail <dsval3@gmail.com>
2025-03-02 15:30:56 -0500

    update html, java, and CSS files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 "use strict"
+
+  var elt = document.getElementById('calculator');
+  var calculator = Desmos.GraphingCalculator(elt);
+
+  var elt = document.getElementById('my-calculator');
+  var calculator = Desmos.GraphingCalculator(elt);

00eb2e59dd20bc6e644922868370b01d9930d7b1
Diane Vail <dsval3@gmail.com>
2025-03-17 20:08:41 -0400

    cleaned up directory and files - working on CSS

--- js/solvingequationtool.js
+++ js/solvingequationtool.js
@@ -14,36 +14,37 @@ let roots = "";
 let intercept1 = "";
 let intercept2 = "";
 
-function checkValues() {  //  Still working on this
-    legA = parseInt(valueA.value);
+function checkValues() { 
+    legA = parseInt(valueA.value);//User enters numbers
     legB = parseInt(valueB.value); 
-    legC = parseInt(valueC.value); //User enters numbers 
+    legC = parseInt(valueC.value);  
 
-    if (legA == 0) {        
+    if (legA == 0) {      //Checks for Linear equation   
         result = "This is a linear equation where the slope is "+ legB +" and the y-intercept is " + legC;
         message.innerText = result;
     }
-    if (legA > 0){
-        let roots = (legB*legB)-(legA*legC*4);
+    if (legA > 0){  //Checks for a positive leading coeff.
+        let roots = (legB * legB)-(4 * legA * legC);  //Checks for imaginary roots
             if (roots < 0){
             result = "The graph is a parabola that opens up, and the x-intercepts are imaginary";
             message.innerText = result;
-            } else {
-                console.log(roots);
-                console.log(legB);
+            
+            } else {  //Solves for x using the Quadriatic Formula                            
+                intercept1 = (-legB + Math.sqrt(roots)) / 2 * legA;                
+                intercept2 = (-legB - Math.sqrt(roots))/ 2 * legA;
                 
-                intercept1 = ((-legB)+(Math.sqrt(roots))/(4*legA))
-                intercept2 = ((-legB)-(Math.sqrt(roots))/(4*legA))
-                console.log(intercept1);
+                
             result = "The graph is a parabola that opens up, and the x-intercepts are "+intercept1+ " and "+intercept2;
             message.innerText = result;
             }
-        } else if (legA < 0){
+        } else if (legA < 0){//Checks for a negative leading coeff.
             let roots = (legB*legB) - (legA*legC*4);
                 if (roots < 0){
                 result = "The graph is a parabola that opens down, and the x-intercepts are imaginary";
                 message.innerText = result;
-            } else {
+            } else { //Solves for x using the Quadriatic Formula
+                intercept1 = (-legB + Math.sqrt(roots)) / 2 * legA;                
+                intercept2 = (-legB - Math.sqrt(roots))/ 2 * legA;
                 result = "The graph is a parabola that opens down, and the x-intercepts are "+intercept1+ " and "+intercept2;
                 message.innerText = result;
                 }
